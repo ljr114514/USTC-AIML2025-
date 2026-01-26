386758edb656d02970ff746f1500595e9b9c5a99
wzh725 <wzh060725@mail.ustc.edu.cn>
2026-01-26 23:57:24 +0800

    opponent_loader.py

--- Final-Project/Final-Project/gobang/opponent_loader.py
+++ Final-Project/Final-Project/gobang/opponent_loader.py
@@ -1,25 +1,27 @@
-import torch.nn as nn
-from typing import *
-from utils import *
-import numpy as np
 import torch
+from utils import *
+from submission import GobangModel
 
 board_size = 12
 bound = 5
 
 
-# Load models using functions 'get_model' without passing any extra
-# parameters, so that we can directly call get_model() in player.py and evaluator.py.
-
-
 def get_opponent():
-    # BEGIN YOUR CODE
-    # from submission import GobangModel
-    # opponent = GobangModel(board_size=board_size, bound=bound)
-    # opponent.load_state_dict(torch.load('opponent.pth'))
-    # return opponent
-    raise NotImplementedError("Not implemented!")
-    # END YOUR CODE
+    """
+    Load and return the trained white-player opponent model.
+    """
+    opponent = GobangModel(board_size=board_size, bound=bound)
+
+    # 修改为你最终保存的对手模型路径
+    opponent_path = "checkpoints/model_final.pth"
+
+    state_dict = torch.load(opponent_path, map_location=device)
+    opponent.load_state_dict(state_dict)
+
+    opponent.to(device)
+    opponent.eval()
+    return opponent
 
 
 __all__ = ['get_opponent']
+
